4395e4a71315057378bac518a02d914a41982488
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-09-19 08:29:23 +0000

    Update client3:core to 3.9.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   lazy val pureConfigCats = "com.github.pureconfig" %% "pureconfig-cats-effect" % "0.17.4"
   lazy val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.17.4"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.16"
-  lazy val sttpClient = "com.softwaremill.sttp.client3" %% "core" % "3.8.16"
+  lazy val sttpClient = "com.softwaremill.sttp.client3" %% "core" % "3.9.0"
   lazy val typeSafeConfig = "com.typesafe" % "config" % "1.4.2"
   lazy val wiremock = "com.github.tomakehurst" % "wiremock-jre8" % "2.35.1"
 }
