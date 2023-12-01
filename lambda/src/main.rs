05ccab2990e253cce096b4616c016ab23d37c4c7
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-12-01 09:48:13 +0000

    DR2-1358 Create anonymiser lambda code.
    I've created a project as a workspace and added in a module for common
    anonymiser code.
    There is a new module for the lambda which uses this code as well as the
    existing script code which uses it too.
    
    There are more tests now and a package with common library functions
    which are shared between the lambda and the script

---
+++ lambda/src/main.rs
@@ -0,0 +1,23 @@
+use aws_lambda_events::event::sqs::SqsEvent;
+use lambda;
+use lambda_runtime::{run, service_fn, Error, LambdaEvent};
+use std::path::PathBuf;
+use tokio;
+
+async fn function_handler(event: LambdaEvent<SqsEvent>) -> Result<(), Error> {
+    for record in event.payload.records.iter() {
+        lambda::process_record(record, PathBuf::from("/tmp"), None, None).await?;
+    }
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_target(false)
+        .without_time()
+        .init();
+
+    run(service_fn(function_handler)).await
+}
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,57 @@
+//!## Anonymiser script
+//!
+//! This is a script to be used to anonymise files on the local disk.
+//!
+//! ## Install
+//! There is an [install.sh](./install.sh) script which will download the latest binary from GitHub and add it to `$HOME/.anonymiser/bin`.
+//! ```bash
+//! curl https://raw.githubusercontent.com/nationalarchives/dr2-court-document-package-anonymiser/main/install.sh | sh
+//! ```
+//! You will need to add `$HOME/.anonymiser/bin` to your $PATH.
+//!
+//! ## Running
+//! ```bash
+//! anonymiser --input /path/to/input --output /path/to/output
+//! ```
+//!
+//! ## Running with docker
+//! ```bash
+//! docker run -v /path/to/input:/input -v /path/to/output:/output public.ecr.aws/u4s1g5v1/anonymiser
+//! ```
+//!
+//! The input path must only contain the tar.gz files you're converting.
+//!
+use anonymise::*;
 use clap::Parser;
-use docx_rs::*;
-use flate2::{read::GzDecoder, write::GzEncoder, Compression};
 use log::{self, LevelFilter};
-use serde_json::{json, Value};
-use sha256::try_digest;
 use simple_logger::SimpleLogger;
-use std::fs::{remove_file, DirEntry};
-use std::io::ErrorKind;
-use std::{fs, fs::File, io, io::Error, io::Read, path::Path, path::PathBuf, process::exit};
-use tar::{Archive, Builder};
+use std::{path::PathBuf, process::exit};
 
-#[derive(Parser)]
-#[clap(name = "anonymiser")]
-struct Opt {
-    /// Input folder
-    #[clap(long, short, value_parser)]
-    input: String,
-
-    /// Output folder
-    #[clap(long, short, value_parser)]
-    output: String,
+/// # The input files and output directory
+struct Files {
+    dir_output: PathBuf,
+    files: Vec<PathBuf>,
 }
 
+/// # Process the input arguments
+///
+/// Returns the `Files` struct with a list of files in the input directory and the output directory as a `PathBuf` struct.
+fn files_from_input_arguments(opt: Opt) -> Files {
+    let dir_input: PathBuf = PathBuf::from(shellexpand::full(&opt.input).unwrap().to_string());
+    let dir_output: PathBuf = PathBuf::from(shellexpand::full(&opt.output).unwrap().to_string());
+    let files = files_in_input_dir(&dir_input).unwrap();
+    Files { dir_output, files }
+}
+
+/// # The entrypoint for the anonymiser script
 fn main() {
     SimpleLogger::new()
         .with_level(LevelFilter::Info)
         .init()
         .unwrap();
-    let opt: Opt = Opt::parse();
-    let dir_input: PathBuf = PathBuf::from(shellexpand::full(&opt.input).unwrap().to_string());
-    let dir_output: PathBuf = PathBuf::from(shellexpand::full(&opt.output).unwrap().to_string());
-    let files: Vec<PathBuf> = files_in_input_dir(&dir_input).unwrap();
-    for file in files {
-        match process_package(&dir_output, &file) {
+    let files_from_input = files_from_input_arguments(Opt::parse());
+    for file in files_from_input.files {
+        match process_package(&files_from_input.dir_output, &file) {
             Ok(_) => {
                 log::info!(
                     "Processed {}",
@@ -47,161 +66,41 @@ fn main() {
     }
 }
 
-fn process_package(dir_output: &PathBuf, file: &PathBuf) -> Result<(), Error> {
-    let tar_gz_file_name: String = file
-        .file_name()
-        .and_then(|name| name.to_os_string().into_string().ok())
-        .ok_or("Error getting the file name from the file")
-        .map_err(|e| Error::new(ErrorKind::InvalidInput, e))?;
+#[cfg(test)]
+mod test {
+    use crate::files_from_input_arguments;
+    use anonymise::Opt;
+    use assert_fs::TempDir;
+    use std::fs::write;
+    use std::path::{Path, PathBuf};
 
-    let output_tar_gz_path: PathBuf =
-        Path::new(&dir_output).join(Path::new(&tar_gz_file_name.replace("TDR", "TST")));
-    let uncompressed_folder_input_path: &PathBuf = &file.with_extension("").with_extension("");
-    let input_batch_reference: &str = uncompressed_folder_input_path
-        .file_name()
-        .and_then(|name| name.to_str())
-        .ok_or(Error::new(
-            ErrorKind::InvalidInput,
-            "Cannot get a batch reference from the file name",
-        ))?;
-    let output_batch_reference: &String = &input_batch_reference.replace("TDR", "TST");
+    #[test]
+    fn test_files_from_input_arguments() {
+        let input_dir = TempDir::new().unwrap();
+        let test_file_names = ["file1", "file2", "file3"];
+        let _ = test_file_names.map(|file_name| {
+            let file_path = input_dir.join(PathBuf::from(file_name));
+            write(file_path.clone(), "".as_bytes()).unwrap();
+            file_path
+        });
+        let input = input_dir.to_str().unwrap().to_string();
+        let output = TempDir::new().unwrap().to_str().unwrap().to_string();
+        let opt = Opt { input, output };
+        let files_result = files_from_input_arguments(opt);
+        let mut files = files_result.files;
 
-    let extracted_output_original_name: PathBuf =
-        dir_output.join(PathBuf::from(&input_batch_reference));
-    let extracted_output_path: PathBuf = dir_output.join(PathBuf::from(output_batch_reference));
+        fn get_file_name(file_path: &Path) -> &str {
+            file_path
+                .file_name()
+                .and_then(|file_name| file_name.to_str())
+                .unwrap()
+        }
 
-    fs::create_dir_all(extracted_output_path.clone())?;
+        let _ = &files.sort();
 
-    decompress_file(&file, &dir_output)?;
-
-    let metadata_input_file_path: &PathBuf = &extracted_output_path.join(PathBuf::from(format!(
-        "TRE-{input_batch_reference}-metadata.json"
-    )));
-    let metadata_output_file_path: &PathBuf = &extracted_output_path.join(PathBuf::from(format!(
-        "TRE-{output_batch_reference}-metadata.json"
-    )));
-
-    if extracted_output_path.exists() {
-        fs::remove_dir_all(&extracted_output_path)?;
+        assert_eq!(files.len(), 3);
+        assert_eq!(get_file_name(&files[0]), "file1");
+        assert_eq!(get_file_name(&files[1]), "file2");
+        assert_eq!(get_file_name(&files[2]), "file3")
     }
-    fs::rename(&extracted_output_original_name, &extracted_output_path)?;
-    fs::rename(&metadata_input_file_path, &metadata_output_file_path)?;
-
-    let mut metadata_json_value: Value = parse_metadata_json(&metadata_output_file_path)?;
-
-    let docx_file_name: &str = metadata_json_value["parameters"]["TRE"]["payload"]["filename"]
-        .as_str()
-        .ok_or("Filename is missing from the metadata json")
-        .map_err(|e| Error::new(ErrorKind::InvalidInput, e))?;
-
-    let judgment_name: String = metadata_json_value["parameters"]["PARSER"]["name"].to_string();
-    let docx_path: PathBuf = extracted_output_path.join(PathBuf::from(docx_file_name));
-
-    create_judgment_docx(&docx_path, judgment_name)?;
-
-    let docx_checksum: String = checksum(&docx_path);
-
-    update_json_file(
-        &metadata_output_file_path,
-        docx_checksum,
-        &mut metadata_json_value,
-    )?;
-
-    if_present_delete(extracted_output_path.join(PathBuf::from(
-        format!("{}.xml", input_batch_reference).as_str(),
-    )))?;
-    if_present_delete(extracted_output_path.join(PathBuf::from("parser.log")))?;
-
-    tar_folder(
-        &output_tar_gz_path,
-        &extracted_output_path,
-        &output_batch_reference,
-    )?;
-
-    fs::remove_dir_all(&extracted_output_path)
-}
-
-fn if_present_delete(path: PathBuf) -> io::Result<()> {
-    if path.exists() {
-        remove_file(path)?
-    }
-    Ok(())
-}
-fn is_not_hidden(entry: &DirEntry) -> bool {
-    entry
-        .file_name()
-        .to_str()
-        .map(|file_name| !file_name.starts_with("."))
-        .unwrap_or(false)
-}
-
-fn is_file(entry: &DirEntry) -> bool {
-    !entry.path().is_dir()
-}
-
-fn files_in_input_dir(directory_path: &PathBuf) -> Result<Vec<PathBuf>, Error> {
-    let path_list: Vec<PathBuf> = fs::read_dir(directory_path)
-        .unwrap()
-        .filter_map(|e| {
-            let entry: DirEntry = e.ok()?;
-            if is_file(&entry) && is_not_hidden(&entry) {
-                Some(entry.path())
-            } else {
-                None
-            }
-        })
-        .collect::<Vec<PathBuf>>();
-    Ok(path_list)
-}
-
-fn checksum(path_string: &PathBuf) -> String {
-    try_digest(path_string).unwrap()
-}
-
-fn tar_folder(
-    tar_path: &PathBuf,
-    path_to_compress: &PathBuf,
-    folder_name: &String,
-) -> Result<(), Error> {
-    let tar_gz: File = File::create(tar_path)?;
-    let enc: GzEncoder<File> = GzEncoder::new(tar_gz, Compression::default());
-    let mut tar: Builder<GzEncoder<File>> = tar::Builder::new(enc);
-    tar.append_dir_all(folder_name, &path_to_compress)?;
-    Ok(())
-}
-
-fn create_judgment_docx(docx_path: &PathBuf, judgment_name: String) -> Result<(), Error> {
-    let file: File = File::create(&docx_path)?;
-    Docx::new()
-        .add_paragraph(Paragraph::new().add_run(Run::new().add_text(judgment_name)))
-        .build()
-        .pack(file)?;
-    Ok(())
-}
-
-fn update_json_file(
-    metadata_file_name: &PathBuf,
-    checksum: String,
-    json_value: &mut Value,
-) -> Result<(), Error> {
-    let tdr: &mut Value = &mut json_value["parameters"]["TDR"];
-    tdr["Contact-Email"] = json!("XXXXXXXXX");
-    tdr["Contact-Name"] = json!("XXXXXXXXX");
-    tdr["Document-Checksum-sha256"] = json!(checksum);
-    fs::write(metadata_file_name, json_value.to_string())
-}
-
-fn decompress_file(path_to_tar: &PathBuf, output_path: &PathBuf) -> Result<(), Error> {
-    let tar_gz: File = File::open(path_to_tar)?;
-    let tar: GzDecoder<File> = GzDecoder::new(tar_gz);
-    let mut archive: Archive<GzDecoder<File>> = Archive::new(tar);
-    archive.unpack(&output_path)?;
-    Ok(())
-}
-
-fn parse_metadata_json(metadata_file_path: &PathBuf) -> Result<Value, Error> {
-    let mut metadata_file: File = File::open(metadata_file_path)?;
-    let mut metadata_json_as_string: String = String::new();
-    metadata_file.read_to_string(&mut metadata_json_as_string)?;
-    Ok(serde_json::from_str(&metadata_json_as_string)?)
 }
