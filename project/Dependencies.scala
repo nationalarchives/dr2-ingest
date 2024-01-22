216264a181a26f26cdd51503f6afac79fe07915c
Sam Palmer <sam.palmer@nationalarchives.gov.uk>
2024-01-22 09:59:32 +0000

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   lazy val mockitoScala = "org.mockito" %% "mockito-scala" % mockitoScalaVersion
   lazy val mockitoScalaTest = "org.mockito" %% "mockito-scala-scalatest" % mockitoScalaVersion
   lazy val preservicaClient = "uk.gov.nationalarchives" %% "preservica-client-fs2" % "0.0.38"
-  lazy val pureConfigCats = "com.github.pureconfig" %% "pureconfig-cats-effect" % "0.17.5"40
+  lazy val pureConfigCats = "com.github.pureconfig" %% "pureconfig-cats-effect" % "0.17.5"
   lazy val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.17.5"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.17"
   lazy val sttpClient = "com.softwaremill.sttp.client3" %% "core" % "3.9.2"
