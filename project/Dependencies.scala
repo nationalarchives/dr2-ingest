8d6c8e571cc519787534e83ddd796568c09f15d3
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-02-22 08:24:30 +0000

    Update log4j-core, ... to 2.23.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   private val mockitoScalaVersion = "1.17.30"
   private val awsLibraryVersion = "1.12.661"
   private val daAwsClientsVersion = "0.1.35"
-  lazy val log4jVersion = "2.22.1"
+  lazy val log4jVersion = "2.23.0"
 
   lazy val authUtils = "uk.gov.nationalarchives" %% "tdr-auth-utils" % "0.0.141"
   lazy val awsDynamoDbClient = "uk.gov.nationalarchives" %% "da-dynamodb-client" % daAwsClientsVersion
