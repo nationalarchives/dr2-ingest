dc7ebed7c8bf8f16ec479c901d279d646cbcd4b8
techncl <nigel.lutale@nationalarchives.gov.uk>
2024-04-05 15:48:27 +0100

    Send Slack deploy messages as one message & fix typo

--- .github/scripts/deploy.py
+++ .github/scripts/deploy.py
@@ -11,11 +11,16 @@ aws_lambda = boto3.client("lambda", region_name="eu-west-2")
 eventbridge = boto3.client("events", region_name="eu-west-2")
 
 
-def send_slack_message(key, icon, status):
-    detail = json.dumps(
-        {"slackMessage": f"{icon} Deploy for {key} with version {version} on environment {environment} {status}"})
+def send_slack_message(slack_message: str):
+    detail = json.dumps({"slackMessage": slack_message})
     eventbridge.put_events(
         Entries=[{'Source': 'DR2IngestDeploy', 'DetailType': 'DR2DevMessage', 'Detail': detail}]
+    )
+
+
+def send_failure_slack_message(key):
+    send_slack_message(
+        f":alert-noflash-slow: Deploy for *{key}* with version *{version}* on environment *{environment}* failure"
     )
 
 
@@ -23,21 +28,27 @@ def update_functions():
     objects = s3.list_objects(Bucket=deploy_bucket, Prefix=version)
     print(f"Found {len(objects)} objects")
     keys = [obj["Key"].replace(f"{version}/", "") for obj in objects["Contents"]]
+    successful_slack_message = f"These are the successful deploys to version *{version}* on environment *{environment}*:\n\n"
     for key in keys:
         print(f"Processing key {key}")
         try:
             aws_lambda.update_function_code(FunctionName=f"{environment}-{key}", S3Bucket=deploy_bucket,
                                             S3Key=f"{version}/{key}")
-            send_slack_message(key, ":green-tick:", "successful")
+            successful_slack_message += f":green-tick: {key}\n"
         except Exception as err:
             print(err)
-            send_slack_message(key, ":alert-noflash-slow:", "failure")
+            send_failure_slack_message(key)
             exit(1)
+    try:
+        send_slack_message(successful_slack_message)
+    except Exception as err:
+        print(err)
+        exit(1)
 
 
 try:
     update_functions()
 except Exception as err:
     print(err)
-    send_slack_message("unkown-lambda", ":alert-noflash-slow:", "failure")
+    send_failure_slack_message("unknown-lambda")
     exit(1)
