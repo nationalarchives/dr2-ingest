4ba4f11801537a0126df9db2507e3744c1530c7b
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-05-22 08:21:56 +0000

    Update aws-java-sdk-secretsmanager to 1.12.727

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   private val sttpVersion = "3.9.6"
   private val circeVersion = "0.14.7"
   private val log4CatsVersion = "2.7.0"
-  private val awsLibraryVersion = "1.12.720"
+  private val awsLibraryVersion = "1.12.727"
   private lazy val scalaTestVersion = "3.2.18"
 
   lazy val awsCrt = "software.amazon.awssdk.crt" % "aws-crt" % "0.29.17"
