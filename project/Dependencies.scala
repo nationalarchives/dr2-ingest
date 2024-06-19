cdf268b9f0185aca701d3d9f1112a9fb3244e9f1
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-06-19 08:26:10 +0000

    Update circe-core, circe-generic, ... to 0.14.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   lazy val daAwsClientsVersion = "0.1.65"
   private val fs2Version = "3.10.2"
   private val sttpVersion = "3.9.7"
-  private val circeVersion = "0.14.7"
+  private val circeVersion = "0.14.8"
   private val log4CatsVersion = "2.7.0"
   private val awsLibraryVersion = "1.12.742"
   private lazy val scalaTestVersion = "3.2.18"
