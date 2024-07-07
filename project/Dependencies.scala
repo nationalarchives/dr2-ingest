62be1a4d4c5a454b6c99c5949f151c8e1a1b9b9e
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-07-07 08:25:49 +0000

    Update aws-crt to 0.30.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   private val awsLibraryVersion = "1.12.753"
   private lazy val scalaTestVersion = "3.2.18"
 
-  lazy val awsCrt = "software.amazon.awssdk.crt" % "aws-crt" % "0.29.17"
+  lazy val awsCrt = "software.amazon.awssdk.crt" % "aws-crt" % "0.30.0"
   lazy val awsLambda = "com.amazonaws" % "aws-java-sdk-lambda" % awsLibraryVersion
   lazy val awsSecretsManager = "com.amazonaws" % "aws-java-sdk-secretsmanager" % awsLibraryVersion
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.5.4"
