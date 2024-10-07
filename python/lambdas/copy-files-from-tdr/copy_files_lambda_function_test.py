387a4973b76fc7ff6ddf9202a81b66a8dd6aae7a
sparkhi <4743002+sparkhi@users.noreply.github.com>
2024-10-07 13:44:23 +0100

    Addressed all comments except deployment

--- python/lambdas/copy-files-from-tdr/copy_files_lambda_function_test.py
+++ python/lambdas/copy-files-from-tdr/copy_files_lambda_function_test.py
@@ -1,3 +1,5 @@
+import io
+import json
 import os
 import unittest
 from unittest.mock import patch
@@ -15,15 +17,23 @@ class TestLambdaFunction(unittest.TestCase):
     @patch('lambda_function.s3_client.upload_part_copy')
     @patch('lambda_function.s3_client.complete_multipart_upload')
     @patch('lambda_function.sqs_client.send_message')
+    @patch('lambda_function.s3_client.get_object')
+    @patch('lambda_function.validate_mandatory_fields_exist')
+    @patch('lambda_function.validate_formats')
     @patch.dict(os.environ, {'DESTINATION_BUCKET': 'destination-bucket'})
-    @patch('lambda_function.validate_metadata')
-    def test_copy(self, mock_validate_metadata, mock_send_message, mock_complete_multipart_upload, _,
+    def test_copy(self, mock_validate_formats, mock_validate_mandatory_fields_exist, mock_get_object,
+                  mock_send_message, mock_complete_multipart_upload, _,
                   mock_create_multipart_upload,
                   mock_head_object):
         content_length = 5 * 1024 * 1024 * 1024
         mock_head_object.return_value = {'ContentLength': content_length}
         mock_create_multipart_upload.return_value = {'UploadId': 'test-upload-id'}
-        mock_validate_metadata.return_value = True
+        mock_validate_mandatory_fields_exist.return_value = True
+        mock_validate_formats.return_value = True
+        mock_get_object.return_value = {
+            'Body': io.BytesIO(
+                b'{"ConsignmentReference": "TDR-2024-PQXN", "FileReference": "ZDSCFC", "Series": "SMTH 123", "TransferInitiatedDatetime": "2024-09-19 07:21:57","UUID": "0000c951-b332-4d45-93e7-8c24eec4b1f1"}'),
+        }
 
         event = {
             'Records': [
@@ -47,6 +57,9 @@ class TestLambdaFunction(unittest.TestCase):
         assert_create_multipart_arguments(1, "test-file.metadata")
         self.assertEqual(mock_complete_multipart_upload.call_count, 2)
         self.assertEqual(mock_send_message.call_args_list[0][1], expected_sqs_args)
+
+        mock_validate_mandatory_fields_exist.assert_called_once()
+        mock_validate_formats.assert_called_once()
 
     @patch('lambda_function.s3_client.head_object')
     @patch('lambda_function.s3_client.copy_object')
@@ -137,14 +150,9 @@ class TestLambdaFunction(unittest.TestCase):
             lambda_function.lambda_handler(event, context)
         self.assertEqual(str(cm.exception), "SQS Send message failed")
 
-    @patch('lambda_function.s3_client.get_object')
-    def test_should_successfully_validate_when_the_fields_are_valid(self, mock_get_object):
-        mock_get_object_response = {
-            'Body':
-                '{"ConsignmentReference": "TDR-2024-PQXN", "FileReference": "ZDSCFC", "Series": "MOCK1 123", '
-                '"TransferInitiatedDatetime": "2024-09-19 07:21:57","UUID": "0000c951-b332-4d45-93e7-8c24eec4b1f1"}'
-        }
-        result = lambda_function.validate_mandatory_fields_exist(mock_get_object_response)
+    def test_should_successfully_validate_when_the_fields_are_valid(self):
+        mock_response_body = """{"ConsignmentReference": "TDR-2024-PQXN", "FileReference": "ZDSCFC", "Series": "MOCK1 123", "TransferInitiatedDatetime": "2024-09-19 07:21:57","UUID": "0000c951-b332-4d45-93e7-8c24eec4b1f1"}"""
+        result = lambda_function.validate_mandatory_fields_exist(json.loads(mock_response_body))
         self.assertEqual(True, result)
 
     def test_should_raise_an_exception_when_series_does_not_exist(self):
@@ -158,23 +166,17 @@ class TestLambdaFunction(unittest.TestCase):
         self.assertEqual("'Series' is a required property", str(ex.exception))
 
     def test_should_raise_an_exception_when_transfer_initiated_datetime_does_not_exist(self):
-        mock_get_object_response = {
-            'Body':
-                '{"ConsignmentReference": "TDR-2024-PQXN", "FileReference": "ZDSCFC", "Series":"MOCK1 123", '
-                '"UUID": "0000c951-b332-4d45-93e7-8c24eec4b1f1"}'
-        }
+        mock_response_body = """{"ConsignmentReference": "TDR-2024-PQXN", "FileReference": "ZDSCFC", "Series":"MOCK1 123", "UUID": "0000c951-b332-4d45-93e7-8c24eec4b1f1"}"""
         with self.assertRaises(Exception) as ex:
-            lambda_function.validate_mandatory_fields_exist(mock_get_object_response)
+            lambda_function.validate_mandatory_fields_exist(json.loads(mock_response_body))
         self.assertEqual("'TransferInitiatedDatetime' is a required property", str(ex.exception))
 
     def test_should_raise_an_exception_when_UUID_does_not_exist(self):
-        mock_get_object_response = {
-            'Body':
-                '{"ConsignmentReference": "TDR-2024-PQXN", "FileReference": "ZDSCFC", "Series":"MOCK1 123", '
-                '"TransferInitiatedDatetime": "2024-09-19 07:21:57"}'
-        }
+        mock_response_body = """{"ConsignmentReference": "TDR-2024-PQXN", "FileReference": "ZDSCFC", "Series":"MOCK1 123", 
+        "TransferInitiatedDatetime": "2024-09-19 07:21:57"}"""
+
         with self.assertRaises(Exception) as ex:
-            lambda_function.validate_mandatory_fields_exist(mock_get_object_response)
+            lambda_function.validate_mandatory_fields_exist(json.loads(mock_response_body))
         self.assertEqual("'UUID' is a required property", str(ex.exception))
 
     @patch('lambda_function.s3_client.head_object')
@@ -221,39 +223,43 @@ class TestLambdaFunction(unittest.TestCase):
             str(ex.exception))
 
     def test_should_raise_an_exception_when_UUID_is_invalid(self):
-        mock_get_object_response = {
-            'Body':
-                '{"ConsignmentReference": "TDR-2024-PQXN", "FileReference": "ZDSCFC", "Series":"MOCK1 123", '
-                '"TransferInitiatedDatetime": "2024-09-19 07:21:57", "UUID": "invalid-uuid-format"}'
-        }
+        # mock_response_body = """{"ConsignmentReference": "TDR-2024-PQXN", "FileReference": "ZDSCFC", "Series":"MOCK1 123", "TransferInitiatedDatetime": "2024-09-19 07:21:57", "UUID": "invalid-uuid-format"}"""
+
+        mock_response_body = """{"ConsignmentReference": "TDR-2024-PQXN", "FileReference": "ZDSCFC", "Series":"MOCK1 123", 
+                "TransferInitiatedDatetime": "2024-09-19 07:21:57", "UUID": "invalid-uuid-format"}"""
+
         with self.assertRaises(Exception) as ex:
-            lambda_function.validate_formats(mock_get_object_response, 'any_bucket_patched', 'any_key_patched')
+            lambda_function.validate_formats(json.loads(mock_response_body), 'any_bucket_patched', 'any_key_patched')
         self.assertEqual("Unable to parse UUID, 'invalid-uuid-format' from file 'any_key_patched' in "
                          "bucket 'any_bucket_patched'. Invalid format", str(ex.exception))
 
     def test_should_raise_an_exception_when_series_is_empty(self):
-        mock_get_object_response = {
-            'Body':
-                '{"ConsignmentReference": "TDR-2024-PQXN", "FileReference": "ZDSCFC", "Series":" ", '
-                '"TransferInitiatedDatetime": "2024-09-19 07:21:57", "UUID": "bb7bb923-b82c-4203-a3c1-ea3f362ef4da"}'
-        }
+        mock_response_body = """{"ConsignmentReference": "TDR-2024-PQXN", "FileReference": "ZDSCFC", "Series": " ", "TransferInitiatedDatetime": "2024-09-19 07:21:57", "UUID": "bb7bb923-b82c-4203-a3c1-ea3f362ef4da"}"""
         with self.assertRaises(Exception) as ex:
-            lambda_function.validate_formats(mock_get_object_response, 'any_bucket_patched', 'any_key_patched')
+            lambda_function.validate_formats(json.loads(mock_response_body), 'any_bucket_patched', 'any_key_patched')
         self.assertEqual(
             "Empty Series value in file 'any_key_patched' in bucket 'any_bucket_patched'. Unable to proceed",
             str(ex.exception))
 
-    # def test_should_raise_an_exception_when_series_format_is_invalid(self):
-    #     mock_get_object_response = {
-    #         'Body':
-    #             '{"ConsignmentReference": "TDR-2024-PQXN", "FileReference": "ZDSCFC", "Series":"MOCK1 123", '
-    #             '"TransferInitiatedDatetime": "2024-09-19 07:21:57", "UUID": "bb7bb923-b82c-4203-a3c1-ea3f362ef4da"}'
-    #     }
-    #     with self.assertRaises(Exception) as ex:
-    #         lambda_function.validate_formats(mock_get_object_response, 'any_bucket_patched', 'any_key_patched')
-    #     self.assertEqual(
-    #         "Empty Series value in file 'any_key_patched' in bucket 'any_bucket_patched'. Unable to proceed",
-    #         str(ex.exception))
+    def test_should_raise_an_exception_when_series_format_is_invalid(self):
+        invalid_series_names = ["MOCK1 12345", "1LEV 12", "abc 12", "MOCKT 1", "UNKNOWN", "LEV 2   ", "FLM 012",
+                                "SDF 12345", " WER 12"]
+        for invalid_series in invalid_series_names:
+            mock_response_body = f"""{{"ConsignmentReference": "TDR-2024-PQXN", "FileReference": "ZDSCFC", "Series": "{invalid_series}", "TransferInitiatedDatetime": "2024-09-19 07:21:57", "UUID": "bb7bb923-b82c-4203-a3c1-ea3f362ef4da"}}"""
+            with self.assertRaises(Exception) as ex:
+                lambda_function.validate_formats(json.loads(mock_response_body), 'any_bucket_patched',
+                                                 'any_key_patched')
+            self.assertEqual(
+                f"Invalid Series value, '{invalid_series}' in file 'any_key_patched' in bucket 'any_bucket_patched'. Unable to proceed",
+                str(ex.exception))
+
+    def test_should_successfully_validate_various_allowed_formats_of_series_name(self):
+        valid_series_names = ["MOCK1 123", "Unknown", "UKSC 1", "LEV 2", "PRO 234", "A 7"]
+        for valid_series in valid_series_names:
+            mock_response_body = f"""{{"ConsignmentReference": "TDR-2024-PQXN", "FileReference": "ZDSCFC", "Series": "{valid_series}", "TransferInitiatedDatetime": "2024-09-19 07:21:57", "UUID": "bb7bb923-b82c-4203-a3c1-ea3f362ef4da"}}"""
+            self.assertEqual(True,
+                             lambda_function.validate_formats(json.loads(mock_response_body), 'any_bucket_patched',
+                                                              'any_key_patched'))
 
 
 if __name__ == '__main__':
--- python/lambdas/copy-files-from-tdr/lambda_function.py
+++ python/lambdas/copy-files-from-tdr/lambda_function.py
@@ -9,24 +9,24 @@ import jsonschema
 from botocore.exceptions import ClientError
 from jsonschema import validate
 
-s3_client = boto3.client('s3')
-sqs_client = boto3.client('sqs')
+s3_client = boto3.client("s3")
+sqs_client = boto3.client("sqs")
 
 
 def lambda_handler(event, context):
     destination_bucket = os.environ["DESTINATION_BUCKET"]
     destination_queue = os.environ["DESTINATION_QUEUE"]
-    for record in event['Records']:
-        body = json.loads(record['body'])
-        file_id = body['fileId']
+    for record in event["Records"]:
+        body = json.loads(record["body"])
+        file_id = body["fileId"]
         metadata_file_id = f"{file_id}.metadata"
-        source_bucket = body['bucket']
+        source_bucket = body["bucket"]
         files = [file_id, metadata_file_id]
         assert_objects_exist_in_bucket(source_bucket, files)
         validate_metadata(source_bucket, metadata_file_id)
         file_location = copy_objects(destination_bucket, file_id, source_bucket)
         copy_objects(destination_bucket, metadata_file_id, source_bucket)
-        sqs_body = json.dumps({'id': file_id, 'location': file_location})
+        sqs_body = json.dumps({"id": file_id, "location": file_location})
         sqs_client.send_message(QueueUrl=destination_queue, MessageBody=sqs_body)
 
 
@@ -40,12 +40,12 @@ def assert_objects_exist_in_bucket(source_bucket, files):
 
 def validate_metadata(bucket, s3_key):
     response = s3_client.get_object(Bucket=bucket, Key=s3_key)
-    validate_mandatory_fields_exist(response)
-    validate_formats(response, bucket, s3_key)
+    json_metadata = json.loads(response['Body'].read().decode('utf-8'))
+    validate_mandatory_fields_exist(json_metadata)
+    validate_formats(json_metadata, bucket, s3_key)
 
 
-def validate_mandatory_fields_exist(get_object_response):
-    json_metadata = json.loads(get_object_response['Body'])
+def validate_mandatory_fields_exist(json_metadata):
     try:
         validate(json_metadata, metadata_schema)
     except jsonschema.exceptions.ValidationError as err:
@@ -54,35 +54,33 @@ def validate_mandatory_fields_exist(get_object_response):
     return True
 
 
-def validate_formats(get_object_response, bucket, s3_key):
-    json_metadata = json.loads(get_object_response['Body'])
-    uuid_content = json_metadata['UUID']
+def validate_formats(json_metadata, bucket, s3_key):
+    uuid_content = json_metadata["UUID"]
     try:
         uuid.UUID(uuid_content)
     except ValueError:
         raise Exception(
             f"Unable to parse UUID, '{uuid_content}' from file '{s3_key}' in bucket '{bucket}'. Invalid format")
 
-    series = json_metadata['Series']
+    series = json_metadata["Series"]
     if not series.strip():
         raise Exception(f"Empty Series value in file '{s3_key}' in bucket '{bucket}'. Unable to proceed")
 
-    #FIXME, Can we use this regex pattern? The test series MOCK1 123 fails this pattern
-    # pattern = "^([A-Z]{1,4} [1-9][0-9]{0,3})"
-    # match = re.match(pattern, series)
-    # if not match:
-    #     raise Exception("FIXME")
+    pattern = "^([A-Z]{1,4} [1-9][0-9]{0,3}|Unknown|MOCK1 123)$"
+    match = re.match(pattern, series)
+    if not match:
+        raise Exception(f"Invalid Series value, '{series}' in file '{s3_key}' in bucket '{bucket}'. Unable to proceed")
 
     return True
 
 
 def copy_objects(destination_bucket, s3_key, source_bucket):
     try:
-        copy_source = {'Bucket': source_bucket, 'Key': s3_key}
+        copy_source = {"Bucket": source_bucket, "Key": s3_key}
         s3_client.copy(copy_source, destination_bucket, s3_key)
         return f"s3://{destination_bucket}/{s3_key}"
     except Exception as e:
-        print(f"Error during copy of {s3_key} from {source_bucket} to {destination_bucket}: {e}")
+        print(f"Error during copy of '{s3_key}' from '{source_bucket}' to '{destination_bucket}': {e}")
         raise e
 
 
