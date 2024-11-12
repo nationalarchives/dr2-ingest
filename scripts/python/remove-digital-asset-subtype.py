70327d229214a9cbacf6067c0540570ef70de7f6
sparkhi <4743002+sparkhi@users.noreply.github.com>
2024-11-12 16:11:24 +0000

    Modified the script to invoke PUT request for update instead of using pyPreservica.

--- scripts/python/remove-digital-asset-subtype.py
+++ scripts/python/remove-digital-asset-subtype.py
@@ -1,15 +1,15 @@
-# This script updates the DigitalAssetSubtype of assets that have already been ingested. This was needed as
-# we had assets coming from 'TDR' had been ingested using a digital asset subtype of 'TDR'
+# This script makes the DigitalAssetSubtype of assets to be empty where the value is 'TDR'. This was needed as
+# we had assets coming from 'TDR', that had been ingested using a digital asset subtype of 'TDR'
 #
 # Implementation: The script makes use of pyPreservica library to connect to Presevica and sequentially invoke
-# various APIs to get the desired effect. It effectively works in the steps as
+# various APIs to get the desired effect. It effectively works in the steps as:
 # 1) Receive a consignment reference as a parameter on command line
 # 2) Based on the consignment reference, get hold of the asset identifiers in it
 # 3) Get the asset entity from the identifiers
 # 4) Find metadata for the asset entity
-# 5) If the DigitalAssetSubtype in the metadata XML is 'TDR', make that element empty
-# 6) Modify the XML to strip out namespace prefixes from everything other than the 'Source' element
-# 7) Call the update_metadata and pass in the modified XML
+# 5) Extract the "source" element as string from the metadata response
+# 6) Modify the "source" element to change the DigitalAssetSubtype to be empty
+# 7) Invoke the PUT method for the same metadata URL with the modified XML
 #
 # Usage: The script can be run in a python environment by passing parameters on the command line
 #     e.g. Usage: python3 remove-digital-asset-subtype.py '<username>' '<password>' '<consignment_reference>'
@@ -18,42 +18,41 @@
 #
 
 import pyPreservica
-import xml.etree.ElementTree
-import re
 import sys
+import requests
 
-SOURCE_NAMESPACE = "http://dr2.nationalarchives.gov.uk/source"
+# Note: The following url is a placeholder, Change it to be the correct url corresponding to your environment
+SERVER = "demo.example.com"
 
 if len(sys.argv) < 4:
     print("Usage: python3 remove-digital-asset-subtype.py '<username>' '<password>' '<consignment_reference>'")
     print("Note: Make sure to enclose the parameters in single quotes to avoid problems with characters like $")
     sys.exit(1)
-
 username = sys.argv[1]
 password = sys.argv[2]
 consignment_reference = sys.argv[3]
 
-# Change the server url to be the correct one corresponding to your environment
-entity_client = pyPreservica.EntityAPI(username, password, None, "demo.example.com")
+entity_client = pyPreservica.EntityAPI(username, password, None, SERVER)
 assets = entity_client.identifier("ConsignmentReference", consignment_reference)
 for asset in assets:
-    print(f"processing {asset.reference}")
+    print(f"Processing: {asset.reference}")
     asset_entity = entity_client.asset(asset.reference)
-    met = entity_client.metadata_for_entity(asset, SOURCE_NAMESPACE)
-    xml_fragment = xml.etree.ElementTree.fromstring(met)
-    namespace = {'ns0': SOURCE_NAMESPACE}
-    old_subtype = xml_fragment.find('ns0:DigitalAssetSubtype', namespaces=namespace)
-    if old_subtype is not None and old_subtype.text == "TDR":
-        old_subtype.text = None
-        #trim whitespaces immediately after '>' in tags
-        modified_xml_string = re.sub(r">\s+", ">", xml.etree.ElementTree.tostring(xml_fragment, encoding="utf-8").decode()).strip()
+    metadata_urls = asset_entity.metadata.keys()
+    for metadata_url in metadata_urls:
+        metadata_response_text = requests.get(metadata_url, headers={"Preservica-Access-Token": f"{entity_client.token}"}).text
 
-        #removenamespace prefix, otherwise this appears on the UI
-        modified_xml_string = modified_xml_string.replace("<ns0:", "<").replace("</ns0:", "</")
+        # ==== Locate the Source fragment by finding the starting ane ending tag in the metadata response
+        source_element_start_index = metadata_response_text.find("<Source")
+        source_element_end_index = metadata_response_text.find("</Source>", source_element_start_index)
+        source_fragment = metadata_response_text[source_element_start_index: source_element_end_index + len("</Source>")]
 
-        #however, the top element 'Source' need to be prefixed with the namespace otherwise we get parsing error
-        modified_xml_string = modified_xml_string.replace("<Source", "<ns0:Source").replace("</Source", "</ns0:Source")
-        entity_client.update_metadata(asset_entity, SOURCE_NAMESPACE, modified_xml_string)
-        print(f"{asset.reference} Done")
-    else:
-        print(f"Old subtype for '{asset.reference}' is not 'TDR', it is '{old_subtype}'")
+        # ==== Modify the extracted fragment and invoke a PUT request
+        if "<DigitalAssetSubtype>TDR</DigitalAssetSubtype>" in  source_fragment:
+            modified_source_fragment = source_fragment.replace("<DigitalAssetSubtype>TDR</DigitalAssetSubtype>", "<DigitalAssetSubtype></DigitalAssetSubtype>")
+            put_response = requests.put(metadata_url, modified_source_fragment, headers={"Content-Type":"application/xml", "Preservica-Access-Token": f"{entity_client.token}"})
+            if put_response.status_code == 200:
+                print(f"DigitalAssetSubtype for {asset.reference} successfully removed")
+            else:
+                print(f"DigitalAssetSubtype update did not succeed for {asset.reference} due to {put_response.text}")
+        else:
+            print(f"DigitalAssetSubtype for {asset.reference} is not 'TDR', nothing to change.")
