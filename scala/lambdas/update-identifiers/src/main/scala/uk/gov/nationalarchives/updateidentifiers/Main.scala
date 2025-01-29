e7d611e6c32733c54432828bc03dd0d080a4f4b4
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2025-01-29 18:21:42 +0000

    Use arguments

--- scala/lambdas/update-identifiers/src/main/scala/uk/gov/nationalarchives/updateidentifiers/Main.scala
+++ scala/lambdas/update-identifiers/src/main/scala/uk/gov/nationalarchives/updateidentifiers/Main.scala
@@ -11,16 +11,17 @@ import uk.gov.nationalarchives.dp.client.EntityClient.EntityType.StructuralObjec
 import uk.gov.nationalarchives.dp.client.fs2.Fs2Client
 
 import java.time.ZonedDateTime
+import scala.concurrent.duration.*
 
 object Main extends IOApp {
 
-  private val since: ZonedDateTime = ZonedDateTime.parse("2025-01-01T00:00:09.000000000Z[Europe/London]")
+  private val since: ZonedDateTime = ZonedDateTime.parse("1990-01-01T00:00:09.000000000Z[Europe/London]")
 
   given Logger[IO] = Slf4jLogger.getLogger[IO]
 
 
   override def run(args: List[String]): IO[ExitCode] =
-    Fs2Client.entityClient("https://tna.preservica.com", "intg-preservica-api-login-details-A1YMZ1t9Qb").flatMap { client =>
+    Fs2Client.entityClient("https://tna.preservica.com", args.head).flatMap { client =>
       def trimIdentifiers(entity: Entity, identifiers: Seq[IdentifierResponse]): IO[Seq[Unit]] =
         identifiers.traverse { identifier =>
           IO.whenA(identifier.value.trim != identifier.value || identifier.identifierName.trim != identifier.identifierName) {
@@ -29,6 +30,7 @@ object Main extends IOApp {
             for {
               _ <- Logger[IO].info(s"Delete,${entity.ref},'${identifier.identifierName.trim}','${identifier.value.trim}'")
               _ <- client.deleteIdentifier(entity, identifier.id)
+              _ <- IO.sleep(10.milliseconds)
               _ <- Logger[IO].info(s"Add,${entity.ref}'${newIdentifier.identifierName}','${newIdentifier.value}'")
               _ <- client.addIdentifierForEntity(entity.ref, entityType, newIdentifier)
             } yield ()
@@ -48,13 +50,13 @@ object Main extends IOApp {
       def update(start: Int): IO[Unit] = {
         for {
           updatedPage <- client.entitiesUpdatedSince(since, start)
-          _ <- Logger[IO].info(s"Found ${updatedPage.length} entities")
+          _ <- Logger[IO].info(s"Found ${updatedPage.length} entities starting from $start")
           _ <- processEntities(updatedPage.filter(_.entityType.contains(StructuralObject)))
           _ <- if updatedPage.isEmpty then IO.unit else update(start + 1000)
         } yield ()
       }
 
-      update(0)
+      update(args.last.toInt)
     }.map(_ => ExitCode.Success).handleErrorWith(err => {
       Logger[IO].error(err)(err.getMessage).map(_ => ExitCode.Error)
     })
