04557dfe94a2a260af6654906dbb79b4b856bb7c
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2025-10-08 12:40:20 +0100

    Review changes

--- runbooks.tf
+++ runbooks.tf
@@ -1,9 +1,13 @@
-module "remove_all_nacl_rules_role" {
-  source = "git::https://github.com/nationalarchives/da-terraform-modules//iam_role"
+locals {
   assume_role_policy = templatefile("${path.module}/templates/iam_role/github_assume_role.json.tpl", {
-    account_id = data.aws_caller_identity.current.account_id,
-  repo_filters = jsonencode(["repo:nationalarchives/dr2-runbooks:environment:intg", "repo:nationalarchives/dr2-runbooks:environment:staging", "repo:nationalarchives/dr2-runbooks:environment:prod"]) })
-  name = "${local.environment}-dr2-runbook-remove-all-nacl-rules"
+    account_id   = data.aws_caller_identity.current.account_id,
+    repo_filters = jsonencode(["repo:nationalarchives/dr2-runbooks:environment:${local.environment}"])
+  })
+}
+module "remove_all_nacl_rules_role" {
+  source             = "git::https://github.com/nationalarchives/da-terraform-modules//iam_role"
+  assume_role_policy = local.assume_role_policy
+  name               = "${local.environment}-dr2-runbook-remove-all-nacl-rules"
   policy_attachments = {
     remove_all_nacl_rules_policy = module.remove_all_nacl_rules_policy.policy_arn
   }
@@ -20,11 +24,9 @@ module "remove_all_nacl_rules_policy" {
 }
 
 module "pause_ingest_role" {
-  source = "git::https://github.com/nationalarchives/da-terraform-modules//iam_role"
-  assume_role_policy = templatefile("${path.module}/templates/iam_role/github_assume_role.json.tpl", {
-    account_id = data.aws_caller_identity.current.account_id,
-  repo_filters = jsonencode(["repo:nationalarchives/dr2-runbooks:environment:intg", "repo:nationalarchives/dr2-runbooks:environment:staging", "repo:nationalarchives/dr2-runbooks:environment:prod"]) })
-  name = "${local.environment}-dr2-runbook-pause-ingest"
+  source             = "git::https://github.com/nationalarchives/da-terraform-modules//iam_role"
+  assume_role_policy = local.assume_role_policy
+  name               = "${local.environment}-dr2-runbook-pause-ingest"
   policy_attachments = {
     pause_ingest_policy = module.pause_ingest_policy.policy_arn
   }
