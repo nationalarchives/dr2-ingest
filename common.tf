6994852af1a83bd777afc58273c51c69d1f42e87
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2025-10-08 12:07:25 +0100

    Code review changes

--- common.tf
+++ common.tf
@@ -74,7 +74,9 @@ locals {
   ]
   retry_statement            = jsonencode([{ ErrorEquals = ["States.ALL"], IntervalSeconds = 2, MaxAttempts = 6, BackoffRate = 2, JitterStrategy = "FULL" }])
   messages_visible_threshold = 1000000
-  cloudflare_ip_ranges       = toset(["173.245.48.0/20", "103.21.244.0/22", "103.22.200.0/22", "103.31.4.0/22", "141.101.64.0/18", "108.162.192.0/18", "190.93.240.0/20", "188.114.96.0/20", "197.234.240.0/22", "198.41.128.0/17", "162.158.0.0/15", "104.16.0.0/13", "104.24.0.0/14", "172.64.0.0/13", "131.0.72.0/22"])
+  # The list comes from https://www.cloudflare.com/en-gb/ips
+  cloudflare_ip_ranges        = toset(["173.245.48.0/20", "103.21.244.0/22", "103.22.200.0/22", "103.31.4.0/22", "141.101.64.0/18", "108.162.192.0/18", "190.93.240.0/20", "188.114.96.0/20", "197.234.240.0/22", "198.41.128.0/17", "162.158.0.0/15", "104.16.0.0/13", "104.24.0.0/14", "172.64.0.0/13", "131.0.72.0/22"])
+  outbound_security_group_ids = [module.outbound_https_access_only.security_group_id, module.outbound_cloudflare_https_access.security_group_id]
 }
 
 data "aws_iam_role" "org_wiz_access_role" {
@@ -181,7 +183,7 @@ module "outbound_https_access_only" {
 
 resource "aws_ec2_managed_prefix_list" "cloudflare_prefix_list" {
   address_family = "IPv4"
-  max_entries    = 15
+  max_entries    = length(local.cloudflare_ip_ranges)
   name           = "${local.environment}-cloudflare-ranges"
   dynamic "entry" {
     for_each = local.cloudflare_ip_ranges
@@ -194,13 +196,13 @@ resource "aws_ec2_managed_prefix_list" "cloudflare_prefix_list" {
 module "outbound_cloudflare_https_access" {
   source      = "git::https://github.com/nationalarchives/da-terraform-modules//security_group"
   common_tags = {}
-  description = "A security group to allow outbound access only"
+  description = "A security group to allow outbound access to Cloudflare IPs only"
   name        = "${local.environment}-outbound-https-to-cloudflare"
   vpc_id      = module.vpc.vpc_id
   rules = {
     egress = [{
       port           = 443
-      description    = "Outbound https cloudflare access",
+      description    = "Outbound https Cloudflare access",
       prefix_list_id = aws_ec2_managed_prefix_list.cloudflare_prefix_list.id
       protocol       = "tcp"
     }]
