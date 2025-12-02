fdc1d36cdc4ad752d9c849737bc21daeaec2c044
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2025-12-02 09:55:25 +0000

    Use variable

--- scala/lambdas/preingest-tdr-package-builder/src/main/scala/uk/gov/nationalarchives/preingesttdrpackagebuilder/Lambda.scala
+++ scala/lambdas/preingest-tdr-package-builder/src/main/scala/uk/gov/nationalarchives/preingesttdrpackagebuilder/Lambda.scala
@@ -59,7 +59,7 @@ class Lambda extends LambdaRunner[Input, Output, Config, Dependencies]:
             assetMetadata <- createAsset(firstPackageMetadata, fileName, originalFilePath, metadataId, potentialMessageId)
             s3FilesMap <- listS3Objects(fileLocation.getHost, assetMetadata.id)
             contentFolderKey <- config.sourceSystem match {
-              case SourceSystem.ADHOC | SourceSystem.PA => IO.pure(s"${firstPackageMetadata.series}/Records")
+              case SourceSystem.ADHOC | SourceSystem.PA => IO.pure(s"${firstPackageMetadata.series}/$defaultFolderName")
               case _ =>
                 IO.fromOption[String](firstPackageMetadata.consignmentReference.orElse(firstPackageMetadata.driBatchReference))(
                   new Exception(s"We need either a consignment reference or DRI batch reference for ${assetMetadata.id}")
