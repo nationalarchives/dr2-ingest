3158a22f7d0682d935a304c60b0e8fbed9ae6fa1
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2026-01-05 13:04:36 +0000

    Remove flow control from the VPC

--- terraform/ingest_flow_control.tf
+++ terraform/ingest_flow_control.tf
@@ -28,8 +28,4 @@ module "dr2_ingest_flow_control_lambda" {
   tags = {
     Name = local.ingest_flow_control_lambda_name
   }
-  vpc_config = {
-    subnet_ids         = module.vpc.private_subnets
-    security_group_ids = [module.outbound_https_access_only.security_group_id, module.outbound_https_access_for_dynamo_db.security_group_id, module.https_to_vpc_endpoints_security_group.security_group_id]
-  }
 }
