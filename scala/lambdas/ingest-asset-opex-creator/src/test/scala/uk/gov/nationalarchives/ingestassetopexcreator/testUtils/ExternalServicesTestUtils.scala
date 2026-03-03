80825dd7ae50571f0c7f75790543672e422781dc
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2026-03-03 16:17:56 +0000

    Add new method in s3 client

--- scala/lambdas/ingest-asset-opex-creator/src/test/scala/uk/gov/nationalarchives/ingestassetopexcreator/testUtils/ExternalServicesTestUtils.scala
+++ scala/lambdas/ingest-asset-opex-creator/src/test/scala/uk/gov/nationalarchives/ingestassetopexcreator/testUtils/ExternalServicesTestUtils.scala
@@ -8,7 +8,7 @@ import org.scanamo.DynamoFormat
 import org.scanamo.request.RequestCondition
 import software.amazon.awssdk.core.async.SdkPublisher
 import software.amazon.awssdk.services.dynamodb.model.{BatchWriteItemResponse, ResourceNotFoundException}
-import software.amazon.awssdk.services.s3.model.{CopyObjectResponse, DeleteObjectsResponse, HeadObjectResponse, ListObjectsV2Response, PutObjectResponse}
+import software.amazon.awssdk.services.s3.model.{CopyObjectResponse, DeleteObjectsResponse, HeadObjectResponse, ListObjectsV2Response, PutObjectResponse, PutObjectTaggingResponse}
 import software.amazon.awssdk.transfer.s3.model.{CompletedCopy, CompletedUpload}
 import uk.gov.nationalarchives.dynamoformatters.DynamoFormatters.FileRepresentationType.PreservationRepresentationType
 import uk.gov.nationalarchives.{DADynamoDBClient, DAS3Client}
@@ -106,6 +106,8 @@ object ExternalServicesTestUtils:
     override def listCommonPrefixes(bucket: String, keysPrefixedWith: String): IO[SdkPublisher[String]] = notImplemented
 
     override def listObjects(bucket: String, prefix: Option[String]): IO[ListObjectsV2Response] = notImplemented
+
+    override def updateObjectTags(bucket: String, key: String, newTags: Map[String, String], potentialVersionId: Option[String]): IO[PutObjectTaggingResponse] = notImplemented
 
   case class Errors(dynamoGetError: Boolean = false, dynamoQueryError: Boolean = false, s3CopyError: Boolean = false, s3UploadError: Boolean = false)
 
