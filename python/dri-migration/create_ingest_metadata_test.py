fe20a65ebfe1f49d69f0cd48da3a63b858ff6d7b
Nigel Lutale <nigel.lutale@nationalarchives.gov.uk>
2026-05-20 18:13:48 +0100

    Write fileId, path and assetId to DB

--- python/dri-migration/create_ingest_metadata_test.py
+++ python/dri-migration/create_ingest_metadata_test.py
@@ -1,15 +1,17 @@
 import hashlib
 import json
 import os
+import sqlite3
 import tempfile
 import unittest
 from pathlib import PureWindowsPath, PurePosixPath
+from sqlite3 import Connection
 from unittest.mock import patch, MagicMock, mock_open, call
 from parameterized import parameterized
 from migrate import create_ingest_metadata
 
 
-def setup_test(mock_checksum, mock_connect, mock_create_skeleton, rows, test_run):
+def setup_test(mock_checksum, mock_connect, mock_create_skeleton, rows, mock_write_to_ic_db, test_run):
     os.environ['CLIENT_LOCATION'] = '/test/client'
     os.environ['STORE_PASSWORD'] = 'password'
     os.environ['DROID_PATH'] = '/test/droid'
@@ -38,9 +40,11 @@ def setup_test(mock_checksum, mock_connect, mock_create_skeleton, rows, test_run
     mock_cursor.fetchmany.side_effect = [rows, []]
     mock_connect.return_value.cursor.return_value = mock_cursor
     mock_checksum.return_value = "abc123"
+    mock_write_to_ic_db.return_value = True
 
 
 class TestMigrate(unittest.TestCase):
+    ic_db_name = "intelligent_caching_test_db.db"
     @parameterized.expand([
         ('tru','test'),
         ('true','abc123'),
@@ -48,6 +52,7 @@ class TestMigrate(unittest.TestCase):
         ('test','test'),
         (None,'abc123'),
     ])
+    @patch("migrate.create_ingest_metadata.write_to_ic_db")
     @patch('oracledb.connect')
     @patch('oracledb.init_oracle_client')
     @patch('builtins.open', new_callable=mock_open, read_data="SELECT * FROM TEST")
@@ -56,7 +61,7 @@ class TestMigrate(unittest.TestCase):
     @patch('migrate.create_ingest_metadata.get_clients')
     def test_migrate_s3_sqs(
             self, test_run, checksum, get_clients, mock_checksum,
-            mock_create_skeleton, _, __, mock_connect
+            mock_create_skeleton, _, __, mock_connect, write_to_ic_db
     ):
         row_fmt = [
             "fmt/123", "uuid-abc", "unitref-abc", "fileid-xyz", "/test/file1", "/dri/a/1/test/file1",
@@ -72,13 +77,13 @@ class TestMigrate(unittest.TestCase):
         ]
         rows = [row_fmt, row_x_fmt]
 
-        setup_test(mock_checksum, mock_connect, mock_create_skeleton, rows, test_run)
+        setup_test(mock_checksum, mock_connect, mock_create_skeleton, rows, write_to_ic_db, test_run)
 
         mock_s3 = MagicMock()
         mock_sqs = MagicMock()
         get_clients.return_value = (mock_s3, mock_sqs,)
 
-        create_ingest_metadata.migrate()
+        create_ingest_metadata.migrate(self.ic_db_name)
 
         if test_run == "true" or test_run is None:
             call_paths = ("/test/file1", "/test/file2")
@@ -124,6 +129,13 @@ class TestMigrate(unittest.TestCase):
             self.assertEqual(rows[idx][1], sent_body["assetId"])
             self.assertEqual("testenv-dr2-ingest-dri-migration-cache", sent_body["bucket"])
 
+        write_to_ic_args = write_to_ic_db.call_args_list[0][0]
+        path = "/test/" if test_run == "true" or test_run is None else ""
+        self.assertEqual([("fileid-xyz", f"{path}file1", "uuid-abc"), ("fileid-xyz", f"{path}file2", "uuid-def")],
+                         write_to_ic_args[0])
+        self.assertEqual(True, isinstance(write_to_ic_args[1], sqlite3.Connection))
+
+    @patch("migrate.create_ingest_metadata.write_to_ic_db")
     @patch('oracledb.connect')
     @patch('oracledb.init_oracle_client')
     @patch('builtins.open', new_callable=mock_open, read_data="SELECT * FROM TEST")
@@ -132,7 +144,7 @@ class TestMigrate(unittest.TestCase):
     @patch('migrate.create_ingest_metadata.get_clients')
     def test_migrate_raises_error_if_consignment_ref_and_batch_ref_are_missing(
             self, get_clients, mock_checksum,
-            mock_create_skeleton, ___, ____, mock_connect,
+            mock_create_skeleton, ___, ____, mock_connect, write_to_ic_db
     ):
         row = [
             "fmt/123", "uuid-abc", "unitref-abc", "fileid-xyz", "/test/file1", "/dri/a/1/test/file1",
@@ -140,13 +152,13 @@ class TestMigrate(unittest.TestCase):
             "series1", "desc1", "desc2", "2021-01-01", None, None,
             "filename.txt", "fileref", "meta", "1", "1", 1, "BornDigital"
         ]
-        setup_test(mock_checksum, mock_connect, mock_create_skeleton, [row], None)
+        setup_test(mock_checksum, mock_connect, mock_create_skeleton, [row], write_to_ic_db, None)
         mock_s3 = MagicMock()
         mock_sqs = MagicMock()
         get_clients.return_value = mock_s3, mock_sqs
 
         with self.assertRaises(ValueError) as cm:
-            create_ingest_metadata.migrate()
+            create_ingest_metadata.migrate(self.ic_db_name)
 
         self.assertEqual("We need either a consignment reference or a dri batch reference", str(cm.exception))
 
@@ -253,6 +265,73 @@ class TestMigrate(unittest.TestCase):
         check_client(s3_client)
         check_client(sqs_client)
 
+    ic_table_name = "dri_files"
+    if os.path.exists(ic_db_name):
+        os.remove(ic_db_name)
 
-if __name__ == '__main__':
+    connection: Connection = sqlite3.connect(ic_db_name)
+    blob_cursor = connection.cursor()
+    blob_cursor.execute(f"""
+        CREATE TABLE "{ic_table_name}" (
+            "file_id"    TEXT NOT NULL UNIQUE,
+            "file_path"  TEXT NOT NULL UNIQUE,
+            "asset_id"   TEXT NOT NULL,
+            PRIMARY KEY("file_id")
+        )"""
+    )
+
+    assets = [
+        ("fileId-abc", "/dri/a/1/test/file1", "assetId-abc"),
+        ("fileId-def", "/dri/a/1/test/file2", "assetId-abc"),
+        ("fileId-ghi", "/dri/a/1/test/file3", "assetId-abc")
+    ]
+
+    def empty_table(self):
+        self.blob_cursor.execute(f"DELETE FROM {self.ic_table_name};")
+
+    def test_writing_to_ic_db(self):
+        self.empty_table()
+        create_ingest_metadata.write_to_ic_db(self.assets, self.connection)
+
+        self.blob_cursor.execute(f"SELECT * FROM {self.ic_table_name};")
+        blob_info = self.blob_cursor.fetchall()
+        self.assertEqual([
+            ("fileId-abc", "/dri/a/1/test/file1", "assetId-abc"),
+            ("fileId-def", "/dri/a/1/test/file2", "assetId-abc"),
+            ("fileId-ghi", "/dri/a/1/test/file3", "assetId-abc")
+        ], blob_info)
+
+    def test_an_exact_duplicate_row_should_not_throw_error_but_just_skip_inserting_into_ic_db(self):
+        self.empty_table()
+        self.blob_cursor.execute(f"INSERT INTO {self.ic_table_name} VALUES ('fileId-abc', '/dri/a/1/test/file1', "
+                                 f"'assetId-abc');")
+
+        create_ingest_metadata.write_to_ic_db(self.assets, self.connection)
+
+        self.blob_cursor.execute(f"SELECT * FROM {self.ic_table_name};")
+        blob_info = self.blob_cursor.fetchall()
+        self.assertEqual([
+            ("fileId-abc", "/dri/a/1/test/file1", "assetId-abc"),
+            ("fileId-def", "/dri/a/1/test/file2", "assetId-abc"),
+            ("fileId-ghi", "/dri/a/1/test/file3", "assetId-abc")
+        ], blob_info)
+
+    def test_a_row_with_non_unique_id_should_fail_to_insert_into_ic_db(self):
+        self.empty_table()
+        self.blob_cursor.execute(f"INSERT INTO {self.ic_table_name} VALUES ('fileId-abc', '/dri/a/1/test/file1', 'assetId-zzz');")
+
+        with self.assertRaises(Exception) as context:
+            create_ingest_metadata.write_to_ic_db(self.assets, self.connection)
+
+        self.assertEqual(f"UNIQUE constraint failed: {self.ic_table_name}.file_path", str(context.exception))
+
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.connection.commit()
+        cls.connection.close()
+        if os.path.exists(cls.ic_db_name):
+            os.remove(cls.ic_db_name)
+
+if __name__ == "__main__":
     unittest.main()
