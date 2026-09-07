710c421d0c43251b7385ed3b455ef4ff8379f338
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2026-09-07 14:12:50 +0100

    Use immutable OIDC

--- terraform/runbooks.tf
+++ terraform/runbooks.tf
@@ -1,7 +1,7 @@
 locals {
   assume_role_policy = templatefile("${path.module}/templates/iam_role/github_assume_role.json.tpl", {
     account_id   = data.aws_caller_identity.current.account_id,
-    repo_filters = jsonencode(["repo:nationalarchives/dr2-runbooks:environment:${local.environment}"])
+    repo_filters = jsonencode(["repo:nationalarchives@10154228/dr2-runbooks@866428977:environment:${local.environment}"])
   })
 }
 
