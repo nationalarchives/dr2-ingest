178276a2dd2a4406256c70b3152aa0478b149060
Sam Palmer <sam.palmer@nationalarchives.gov.uk>
2026-09-08 11:39:11 +0100

    Use immutable OIDC (#1237)
    * Use immutable OIDC
    
    * Add s3 bucket and key for queue creator
    
    * Ran terraform fmt
    
    * Consistent naming for runbooks file.
    
    Also added a moved block for the queue creator

--- terraform/custodial_copy_queue_creator.tf
+++ terraform/custodial_copy_queue_creator.tf
@@ -45,4 +45,12 @@ module "dr2_custodial_copy_queue_creator_lambda" {
     PRESERVICA_SECRET_NAME = aws_secretsmanager_secret.preservica_read_metadata.name
     OUTPUT_QUEUE_URL       = module.dr2_custodial_copy_queue.sqs_queue_url
   }
+  s3_bucket = local.code_deploy_bucket
+  s3_key    = "${var.lambda_code_version}/custodial-copy-queue-creator"
+}
+
+
+moved {
+  from = module.dr2_custodial_copy_queue_creator_lambda.aws_lambda_function.lambda_function[0]
+  to   = module.dr2_custodial_copy_queue_creator_lambda.aws_lambda_function.lambda_function_s3[0]
 }
