87a725f62402ac0f28b8ba30b81cf0ac3c66247c
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2026-09-24 16:19:05 +0100

    Various changes
    * Set the headers on the gh api call. It complains about the inputs if you don't
    * Correct min -> main typo
    * Run the jobs in their environments otherwise ${{ secrets.ACCOUNT_NUMBER }} returns nothing.
    * Check out the submodules to get da-terraform-configurations
    * Check out tags so finding the version tag step works;
    * Get it to run on a cron. This can't be tested properly until it's merged so we'll see.

--- terraform/common.tf
+++ terraform/common.tf
@@ -234,7 +234,7 @@ module "vpc" {
     account_id = data.aws_caller_identity.current.account_id
   })
 
-  interface_endpoints = var.disable_networking ? {
+  interface_endpoints = var.disable_networking ? {} : {
     secretsmanager = {
       name = "com.amazonaws.${local.aws_region_name}.secretsmanager",
       policy = templatefile("${path.module}/templates/vpc/default_endpoint_policy.json.tpl", {
@@ -280,7 +280,7 @@ module "vpc" {
       security_group_ids = [module.interface_endpoints_security_group.security_group_id]
       enable_private_dns = true
     },
-  } : {}
+  }
 }
 
 data "aws_eip" "eip" {
